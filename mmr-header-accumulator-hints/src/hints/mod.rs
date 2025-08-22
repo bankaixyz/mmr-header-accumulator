7212f749f8b1ad7fd958e3adcfbac3e0de427cc8
petscheit <etscheitp@gmail.com>
2025-08-22 17:52:05 +0200

    chore: expose all non-default hints via unified function

--- mmr-header-accumulator-hints/src/hints/mod.rs
+++ mmr-header-accumulator-hints/src/hints/mod.rs
@@ -1,21 +1,23 @@
+use std::collections::HashMap;
+
+use cairo_vm_base::default_hints::HintImpl;
+
+
+use crate::hints::mmr::{
+    hint_is_position_in_mmr_array, mmr_bit_length, mmr_left_child,
+    HINT_IS_POSITION_IN_MMR_ARRAY, MMR_BIT_LENGTH, MMR_LEFT_CHILD,
+};
+
 pub mod input;
 pub mod mmr;
 
-// pub fn run_hint(
-//     vm: &mut VirtualMachine,
-//     exec_scope: &mut ExecutionScopes,
-//     hint_data: &HintProcessorData,
-//     constants: &HashMap<String, Felt252>,
-// ) -> Result<(), HintError> {
-//     let hints = [print::run_hint, sha::run_hint];
-
-//     for hint in hints.iter() {
-//         let res = hint(vm, exec_scope, hint_data, constants);
-//         if !matches!(res, Err(HintError::UnknownHint(_))) {
-//             return res;
-//         }
-//     }
-//     Err(HintError::UnknownHint(
-//         hint_data.code.to_string().into_boxed_str(),
-//     ))
-// }
+pub fn get_hints() -> HashMap<String, HintImpl> {
+    let mut hints = HashMap::<String, HintImpl>::new();
+    hints.insert(MMR_BIT_LENGTH.to_string(), mmr_bit_length);
+    hints.insert(MMR_LEFT_CHILD.to_string(), mmr_left_child);
+    hints.insert(
+        HINT_IS_POSITION_IN_MMR_ARRAY.to_string(),
+        hint_is_position_in_mmr_array,
+    );
+    hints
+}
